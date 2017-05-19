0d60993727de69caaaaf371fc47af1036b881c58
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-05-19 11:20:41 +0200

    Add lDap Sample

---
+++ .net/Ldap/Ldap.NetCore.Console/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Ldap.NetCore.Console
+{
+    public class AppUser
+    {
+        public string DisplayName { get; set; }
+
+        public string Username { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
