d8efe36cc60d3c3571d9c24fb81d7f77da7c0db7
trenoncourt <thibaut.renoncourt@gmail.com>
2017-05-28 11:57:20 +0200

    Add Resource Owner Password Credentials Grant Flow sample project.

---
+++ .net/OpenID/OpenIdFlows/OpenIdFlows.ResourceOwnerPasswordCredentialsGrantFlow.Sample/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace OpenIdFlows.ResourceOwnerPasswordCredentialsGrantFlow.Sample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                loggerFactory.AddConsole(); 
+            }
+        }
+    }
+}
