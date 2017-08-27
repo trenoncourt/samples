1a84b35efa1445c10a89e16718172a219e32d8aa
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-08-27 14:17:52 +0200

    swagger

---
+++ .net/Swagger/Nswag.NetCore.Api/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nswag.NetCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpPost()]
+        public Test Get([FromBody] Test id)
+        {
+            return null;
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Test
+    {
+        /// <summary>
+        /// Test de commentaire principal.
+        /// </summary>
+        public Toto Toto { get; set; }
+
+        
+        public DateTime? MyProperty { get; set; }
+
+        public string Titi { get; set; }
+
+
+        public int Tutu { get; set; }
+    }
+
+    public enum Toto
+    {
+        /// <summary>
+        /// Test de commentaire.
+        /// </summary>
+        T = 1,
+        U = 2
+    }
+}
---
+++ .net/Swagger/Nswag.NetCore.Api/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NSwag.AspNetCore;
+using System.Reflection;
+
+namespace Nswag.NetCore.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvcCore();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                loggerFactory.AddConsole(Configuration.GetSection("Logging")); 
+            }
+
+            app.UseSwaggerUi(typeof(Startup).GetTypeInfo().Assembly, new SwaggerUiOwinSettings());
+
+            app.UseMvc();
+        }
+    }
+}
